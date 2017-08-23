edb2eb9e7a17e0b89cf0f61f8cba77bb07e79b0b
kikorose <nakelia78@gmil.com>
2017-08-23 14:18:02 -0400

    first commit(Database Relationships one to many)

---
+++ src/main/java/com/example/demo/DemoSpringBoot14Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot14Application.class, args);
+	}
+}
